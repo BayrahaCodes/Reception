afcd6e47ffab8e8a6e3b501fe2cfcaa9515947d1
Safwan <bayrahainternational@gmail.com>
2026-04-04 05:11:08 +0600

    ready for Render

--- receivetion/wsgi.py
+++ receivetion/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import gunicorn
 from django.core.wsgi import get_wsgi_application
+
+import receivetion
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'receivetion.settings')
 
 application = get_wsgi_application()
+
+

e0d646d879c6179c552b3429b5a1716c04bc9669
BayrahaCodes <bayrahainternational@gmail.com>
2026-04-07 04:20:52 +0600

    Update urls.py

--- receivetion/urls.py
+++ receivetion/urls.py
@@ -1,9 +1,10 @@
+
+
 from django import views
 from django.contrib import admin
 from django.urls import path
 from .views import home,register,events,gallery,ideas,contact, success,register_user
 from . import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,11 +17,4 @@ urlpatterns = [
     path('gallery/', gallery, name='gallery'),
     path('ideas/', ideas, name='ideas'),
     path('contact/', contact, name='contact'),
-    path('send-mails/', views.permession_mail, name='send_mails'),
-    path('take-idea/', views.take_idea, name='take_idea'),
-    path("invitation/",views.invitation,name="invitation_mail"),
-    path("club_invitation/",views.club_invitation,name="club_invitation"),
-    path("club_permission/",views.permession_mail,name="club_permission"),
-    path("senior_invitation/",views.send_cr_mail,name="senior_invitation"),
-    path("batch_invitation/",views.send_bachmate_mail,name="batch_invitation"),
 ]
